2b14ab4a20279df1c98d0db1b54a4a111b467198
MNLaugh <contact@mnlaugh.com>
2023-05-11 01:36:40 +0200

    First commit [get repos, rate limit protection]

---
+++ mod.d.ts
@@ -0,0 +1,162 @@
+export type GithubApiOptions = {
+  entrypoint?: string;
+  token?: string;
+  orgs?: string[];
+  me: string;
+}
+
+export type OrgReposOptions = {
+    type?: "all" | "public" | "private" | "forks" | "sources" | "member" | "internal";
+    sort?: "created" | "updated" | "pushed" | "full_name";
+    direction?: "asc" | "desc";
+    per_page?: number;
+    page?: number;
+}
+
+export type UserReposOptions = OrgReposOptions & {
+    visibility?: "all" | "public" | "private",
+    affiliation?: string,
+    type?: "all" | "owner" | "public" | "private" | "member",
+    since?: string,
+    before?: string,
+}
+
+export type Language = {
+  name: string;
+  files: number;
+  percentage: number;
+}
+
+export type RepoMini = {
+  name: string;
+  full_name: string;
+  description: string;
+  html_url: string;
+  homepage: string | null;
+  languages: Language[];
+  topics: string[];
+  release: {
+    name: string;
+    url: string;
+  } | null;
+  license: string | null;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export type Owner = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+export type License = {
+  key: string;
+  name: string;
+  spdx_id: string;
+  url: string;
+  node_id: string;
+}
+
+export type Repo = {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  owner: Owner,
+  html_url: string;
+  description: string;
+  fork: boolean;
+  url: string;
+  forks_url: string;
+  keys_url: string;
+  collaborators_url: string;     
+  teams_url: string;
+  hooks_url: string;
+  issue_events_url: string;
+  events_url: string;
+  assignees_url: string;
+  branches_url: string;
+  tags_url: string;
+  blobs_url: string;
+  git_tags_url: string;
+  git_refs_url: string;
+  trees_url: string;
+  statuses_url: string;
+  languages_url: string;
+  stargazers_url: string;
+  contributors_url: string;
+  subscribers_url: string;
+  subscription_url: string;
+  commits_url: string;
+  git_commits_url: string;
+  comments_url: string;
+  issue_comment_url: string;
+  contents_url: string;
+  compare_url: string;
+  merges_url: string;
+  archive_url: string;
+  downloads_url: string;
+  issues_url: string;
+  pulls_url: string;
+  milestones_url: string;
+  notifications_url: string;
+  labels_url: string;
+  releases_url: string;
+  deployments_url: string;
+  created_at: Date;
+  updated_at: Date;
+  pushed_at: Date;
+  git_url: string;
+  ssh_url: string;
+  clone_url: string;
+  svn_url: string;
+  homepage: string | null;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string;
+  has_issues: boolean;
+  has_projects: boolean;
+  has_downloads: boolean;
+  has_wiki: boolean;
+  has_pages: boolean;
+  forks_count: number;
+  mirror_url: URL | null;
+  archived: boolean;
+  disabled: boolean;
+  open_issues_count: number;
+  license: null | License;
+  allow_forking: boolean;
+  is_template: boolean;
+  web_commit_signoff_required: boolean;
+  topics: string[],
+  visibility: string;
+  forks: number;
+  open_issues: number;
+  watchers: number;
+  default_branch: string;
+  permissions: {
+    admin: boolean;
+    maintain: boolean;
+    push: boolean;
+    triage: boolean;
+    pull: boolean;
+  }
+}
---
+++ mod.ts
@@ -0,0 +1,114 @@
+import type {
+  OrgReposOptions,
+  UserReposOptions,
+  GithubApiOptions,
+  Repo,
+} from "./mod.d.ts";
+
+export default class GithubApi {
+  ratelimitRemaining: number;
+  rateLimitResetDate?: Date;
+  entrypoint: string;
+  #token?: string;
+  orgs: string[];
+  me: string;
+  constructor({
+    entrypoint = "https://api.github.com",
+    token,
+    orgs = [],
+    me,
+  }: GithubApiOptions) {
+    this.ratelimitRemaining = 0;
+    this.entrypoint = entrypoint;
+    this.#token = token || Deno.env.get("GITHUB_TOKEN");
+    this.orgs = orgs;
+    this.me = me;
+
+    if (!this.#token) throw new Error("No token provided");
+  }
+
+  url(path: string) {
+    return new URL(`${this.entrypoint}${path}`);
+  }
+
+  // deno-lint-ignore no-explicit-any
+  async get(url: URL | string, options = {}): Promise<any> {
+    const res = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/vnd.github+json",
+        Authorization: `token ${this.#token}`,
+      },
+      ...options,
+    }); 
+    if (!res.ok) throw new Error(`failed to fetch ${url}\n${res.status} ${res.statusText}`);
+    this.ratelimitRemaining = Number(res.headers.get("x-ratelimit-remaining"));
+    if (this.rateLimitResetDate === new Date(Number(res.headers.get("x-ratelimit-reset")) * 1000)) this.rateLimitResetDate = new Date(Number(res.headers.get("x-ratelimit-reset")) * 1000);
+    if (this.ratelimitRemaining < 1) throw new Error(`rate limit exceeded`);
+    return res.json();
+  }
+
+  urlSearchParams(options?: Record<string, number | string>): string {
+    if (!options) return "";
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(options)) {
+      params.set(key, String(value));
+    }
+    return `?${params.toString()}`;
+  }
+
+  // deno-lint-ignore no-explicit-any
+  parseRepo(repo: Record<string, any>): Repo {
+    for (const [key, value] of Object.entries(repo)) {
+      if (key.search("_at") !== -1) repo[key] = new Date(value);
+      else if (
+        (key === "owner" && typeof value === "object") ||
+        (key === "license" && typeof value === "object" && value !== null)
+      ) repo[key] = this.parseRepo(value);
+    }
+    return (repo as Repo);
+  }
+
+  // deno-lint-ignore no-explicit-any
+  async test(): Promise<any> {
+    const res = await this.get(this.url("/rate_limit"));
+    console.log(res);
+  }
+
+  async userRepos(options?: {
+    username?: string,
+    options?: UserReposOptions,
+  }): Promise<Repo[]> {
+    let _repos = [];
+    const repos: Repo[] = [];
+    const params = this.urlSearchParams(options?.options);
+    if (!options?.username) _repos = await this.get(this.url(`/user/repos${params}`));
+    else _repos = await this.get(this.url(`/users/${options.username}/repos${params}`));
+    for (const repo of _repos) repos.push(this.parseRepo(repo));
+    return repos;
+  }
+
+  async orgRepos({
+    org,
+    options = {},
+  }: {
+    org?: string,
+    options?: OrgReposOptions,
+  }): Promise<Repo[]> {
+    let _repos = [];
+    const repos: Repo[] = [];
+    const params = this.urlSearchParams(options);
+    if (!org) _repos = await this.get(this.url(`/user/repos${params}`));
+    else _repos = await this.get(this.url(`/orgs/${org}/repos${params}`));
+    for (const repo of _repos) repos.push(this.parseRepo(repo));
+    return repos;
+  }
+
+  // deno-lint-ignore no-explicit-any
+  async hooks({ username = this.me, repo }: { username?: string, repo: string }): Promise<any> {
+    const hooks: Record<string, string> = {};
+    const _hooks = await this.get(this.url(`/repos/${username}/${repo}/hooks`));
+    console.log(_hooks);
+    return hooks;
+  }
+}
